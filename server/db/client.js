0126690acf22df469e589e0ddc9d22408b0daa50
Kirby S <lackman.kirby@gmail.com>
2023-12-17 10:17:46 -0600

    Updated postgres client to connect to database url environment variable

--- server/db/client.js
+++ server/db/client.js
@@ -2,8 +2,7 @@
 const { Client } = require('pg')
 
 //Establishing connect to database (like how we do with http://)
-const dbName = 'schuetzsite'
-const client = new Client(`postgres://localhost:5497/schuetzsite`)
+const client = new Client(process.env.DATABASE_URL);
 
 //Export for use in other files
 module.exports = client;
