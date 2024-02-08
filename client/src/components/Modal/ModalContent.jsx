b30fa61372ef8d9891f730e22cb294387ea67d42
Kirby S <lackman.kirby@gmail.com>
2024-02-08 11:20:03 -0600

    working contact card

--- client/src/components/Modal/ModalContent.jsx
+++ client/src/components/Modal/ModalContent.jsx
@@ -1,35 +1,37 @@
 import { Document, Page } from 'react-pdf';
 import React from 'react';
-import { downloadIcon } from ".../assets/downloadIcon.png";
-import { githubIcon } from ".../assets/githubIcon.png";
-import { linkedinIcon } from ".../assets/linkedinIcon.png";
+import { usePDF } from 'react-to-pdf';
 import { pdfjs } from 'react-pdf';
 import schuetzResume from '../../assets/schuetzResume.pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
+
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
+
 export default function ModalContent() {
-  // const { toPDF, targetRef } = usePDF({filename: 'schuetzResume.pdf'}); // Commented out as it's not used
-  return (
-    <div className="contactCard">
-      <h1>contact card</h1>
-      <a href="https://www.linkedin.com/in/kirby-schuetz" target="_blank" rel="noopener noreferrer">
-        <img src={linkedinIcon} alt="linkedin link" width="25" height="25"/>
-        linkedin.com/kirby-schuetz/
-      </a>
-      <a href="https://github.com/Kirby-Schuetz" target="_blank" rel="noopener noreferrer">
-        <img src={githubIcon} alt="github link" width="25" height="25"/>
-        github.com/Kirby-Schuetz
-      </a>
-      <button onClick={() => alert('Download button clicked')}>
-        <img src={downloadIcon} alt="download resume link" width="25" height="25"/>
-        Download Resume
-      </button>
-      <Document file={schuetzResume}>
-        <Page pageNumber={1} style={{ width: 'auto', height: '100%' }}/>
-      </Document>
-    </div>
-  );
+  const { toPDF, targetRef } = usePDF({filename: 'schuetzResume.pdf'});
+    return (
+        <div className="contactCard">
+          <h1>contact card</h1>
+          <button onClick={(e) => {e.preventDefault();
+            window.location.href='https://www.linkedin.com/in/kirby-schuetz';
+            }}
+            > linkedin.com/kirby-schuetz/</button>
+          <button onClick={(e) => {e.preventDefault();
+            window.location.href='https://github.com/Kirby-Schuetz';
+            }}
+            > github.com/Kirby-Schuetz</button>
+          <button onClick={() => toPDF()}>Download Resume</button>
+          <Document
+            file={ schuetzResume }
+            >
+            <Page             
+            pageNumber={1} 
+            style={{ width: 'auto', height: '100%' }}/>
+          </Document>
+        </div>
+        
+    );
 }
