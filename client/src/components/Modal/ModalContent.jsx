10b25bcc90c7ffb2196b2780e5070795945bf491
Kirby S <lackman.kirby@gmail.com>
2024-02-08 11:32:11 -0600

    moved icons and added to contact card

--- client/src/components/Modal/ModalContent.jsx
+++ client/src/components/Modal/ModalContent.jsx
@@ -2,6 +2,9 @@ import { Document, Page } from 'react-pdf';
 import React from 'react';
 import { usePDF } from 'react-to-pdf';
 import { pdfjs } from 'react-pdf';
+import linkedinIcon from '../Modal/linkedinIcon.png';
+import githubIcon from "../Modal/githubIcon.png";
+import downloadIcon from "../Modal/downloadIcon.png";
 import schuetzResume from '../../assets/schuetzResume.pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import "react-pdf/dist/esm/Page/TextLayer.css";
@@ -15,15 +18,18 @@ export default function ModalContent() {
     return (
         <div className="contactCard">
           <h1>contact card</h1>
-          <button onClick={(e) => {e.preventDefault();
+          <img src={linkedinIcon} alt="linkedin link" 
+          onClick={(e) => {e.preventDefault();
             window.location.href='https://www.linkedin.com/in/kirby-schuetz';
             }}
-            > linkedin.com/kirby-schuetz/</button>
-          <button onClick={(e) => {e.preventDefault();
+            />
+          <img src={githubIcon} alt="github link" 
+          onClick={(e) => {e.preventDefault();
             window.location.href='https://github.com/Kirby-Schuetz';
             }}
-            > github.com/Kirby-Schuetz</button>
-          <button onClick={() => toPDF()}>Download Resume</button>
+            />
+          <img src={downloadIcon} alt="download resume" 
+          onClick={() => toPDF()}/>
           <Document
             file={ schuetzResume }
             >
