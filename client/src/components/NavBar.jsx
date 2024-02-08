485723965e83945a712b1368d2b304f51b043ab0
Kirby S <lackman.kirby@gmail.com>
2024-02-08 10:19:07 -0600

    navbar adjustments

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,14 +2,11 @@ import { Link } from "react-router-dom";
 import learnLogo from "../assets/learnLogo.png";
 import backgroundTile from "../assets/backgroundTile.png";
 import { useState } from "react";
-// import LogoutButton from "../components/LogoutPage";
-import {useLogin} from "../components/Context/LoginContext";
-
-import "../App.css"; // Replace with the correct path to your CSS file
+import { useLogin } from "../components/Context/LoginContext";
+import "../App.css"; // Replace with path to CSS file
 
 export default function NavBar() {
-    const { isLoggedIn } = useLogin();
-
+  const { isLoggedIn } = useLogin();
   const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   const toggleNav = () => {
@@ -21,51 +18,44 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="navbar">
-      <div className="logo-container">
-      <img
-            className="backgroundTile"
-            src={backgroundTile}
-            // alt="learning techodyssey logo"
-            // width="200"
-            // height="300"
-          />
-          </div>
-          <button className="hamburger" onClick={toggleNav}>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </button>
-          <div className="nav-container">
-          <div className={`nav-links ${isNavExpanded ? 'expanded' : ''}`}>
-          <div className="background-container">
-      <img
-            className="navLogo"
-            src={learnLogo}
-            alt="learning techodyssey logo"
-            width="200"
-            height="300"
-          />
-          </div>
-            <Link to="/" className="navLogo" onClick={closeNav}>
-              <img
-              src={learnLogo}
-              alt="learning techodyssey logo"
-              width="200"
-              height="300"
-              />
-            </Link>            
-            <Link to="/blogs" onClick={closeNav}>Blog</Link>
-            <Link to="/projects" onClick={closeNav}>Projects</Link>
-            <Link to="/reviews" onClick={closeNav}>Reviews</Link>
-            <Link to="/contact" onClick={closeNav}>Contact</Link>
-            {isLoggedIn ? (
-              <Link to="/logout" onClick={closeNav}>Logout</Link>
-            ):(
-            <Link to="/login" onClick={closeNav}>Login</Link>
-            )}
-            <a href="#" className="close" onClick={closeNav}></a>
-      </div >
+    <nav className={`navbar ${isNavExpanded ? "expanded" : ""}`}>
+      <div className="background-container">
+        <img className="backgroundTile" src={backgroundTile} alt="background" />
+      </div>
+      <button className="hamburger" onClick={toggleNav}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+      <div className="menu-text">{isNavExpanded ? null : "menu"}</div>
+      <div className="nav-container">
+        <div className={`nav-links ${isNavExpanded ? "expanded" : ""}`}>
+          <Link to="/" className="navLogo" onClick={closeNav}>
+            <img src={learnLogo} alt="learning techodyssey logo" width="200" height="300" />
+          </Link>
+          <Link to="/blogs" onClick={closeNav}>
+            blog
+          </Link>
+          <Link to="/projects" onClick={closeNav}>
+            projects
+          </Link>
+          <Link to="/reviews" onClick={closeNav}>
+            reviews
+          </Link>
+          <Link to="/contact" onClick={closeNav}>
+            contact
+          </Link>
+          {isLoggedIn ? (
+            <Link to="/logout" onClick={closeNav}>
+              logout
+            </Link>
+          ) : (
+            <Link to="/login" onClick={closeNav}>
+              login
+            </Link>
+          )}
+          <a href="#" className="close" onClick={closeNav}></a>
+        </div>
       </div>
     </nav>
   );
