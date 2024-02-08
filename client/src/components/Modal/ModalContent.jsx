c295958cc95bd04e3d5fbb993d14da8cb01d5734
Kirby S <lackman.kirby@gmail.com>
2024-02-08 11:14:00 -0600

    previous failed. adding in usePDF

--- client/src/components/Modal/ModalContent.jsx
+++ client/src/components/Modal/ModalContent.jsx
@@ -1,9 +1,9 @@
 import { Document, Page, pdfjs } from 'react-pdf';
-// import React from 'react';
+import React from 'react';
 import downloadIcon from ".../assets/downloadIcon.png";
 import githubIcon from ".../assets/githubIcon.png";
 import linkedinIcon from ".../assets/linkedinIcon.png";
-// import { usePDF } from 'react-to-pdf';
+import { usePDF } from 'react-to-pdf';
 import schuetzResume from '../../assets/schuetzResume.pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import "react-pdf/dist/esm/Page/TextLayer.css";
@@ -12,24 +12,33 @@ pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.vers
 
 
 export default function ModalContent() {
-  // const { toPDF, targetRef } = usePDF({filename: 'schuetzResume.pdf'}); // This hook is commented out to test if needed
-  return (
-    <div className="contactCard">
-      <h1>contact card</h1>
-      <button onClick={() => window.location.href='https://www.linkedin.com/in/kirby-schuetz'}>
-        <img src={linkedinIcon} alt="linkedin link" width="25" height="25"/>
-      </button>
-      <button onClick={() => window.location.href='https://github.com/Kirby-Schuetz'}>
-        <img src={githubIcon} alt="github link" width="25" height="25"/>
-      </button>
-      {/* <button onClick={() => toPDF()}> */}
-      <button onClick={() => alert('Download button clicked')}> {/* Placeholder for the download action */}
-        <img src={downloadIcon} alt="download resume link" width="25" height="25"/>
-      </button>
-      {/* <Document file={ schuetzResume }> */}
-      <Document file={schuetzResume} onLoadSuccess={console.log('Document loaded successfully')}>
-        <Page pageNumber={1} style={{ width: 'auto', height: '100%' }}/>
-      </Document>
-    </div>
-  );
+  const { toPDF, targetRef } = usePDF({filename: 'schuetzResume.pdf'});
+    return (
+        <div className="contactCard">
+          <h1>contact card</h1>
+          <button onClick={(e) => {e.preventDefault();
+            window.location.href='https://www.linkedin.com/in/kirby-schuetz';
+            }}
+            > 
+            <img src={linkedinIcon} alt="linkedin link" width="25" height="25"/>
+            </button>
+          <button onClick={(e) => {e.preventDefault();
+            window.location.href='https://github.com/Kirby-Schuetz';
+            }}
+            > 
+            <img src={githubIcon} alt="github link" width="25" height="25"/>
+            </button>
+          <button onClick={() => toPDF()}>
+          <img src={downloadIcon} alt="download resume link" width="25" height="25"/>
+          </button>
+          <Document
+            file={ schuetzResume }
+            >
+            <Page             
+            pageNumber={1} 
+            style={{ width: 'auto', height: '100%' }}/>
+          </Document>
+        </div>
+        
+    );
 }
